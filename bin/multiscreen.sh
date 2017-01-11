5a342f11d5ba7336d1d4a5d761655d5b2221da4f
Maciej Kobuszewski <maciej.kobuszewski@gmail.com>
2017-01-12 00:48:54 +0100

    init

---
+++ bin/multiscreen.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#xrandr --output HDMI1 --mode 1920x1080 --right-of HDMI2 --output HDMI2 --mode 1920x1080 --output VGA1 --mode 1920x1080 --right-of HDMI1
+xrandr --output HDMI1 --mode 1920x1080 --output VGA1 --mode 1920x1080 --right-of HDMI1
---
+++ bin/rotate_screen.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+xrandr --output eDP1 --off
+xrandr --output HDMI1 --rotate left
+xrandr --output HDMI1 --mode 1280x1024
